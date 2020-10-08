557b393fd3be0ca1b3ee14f2553d70da72b6e36e
Jorge Casanova <jcasanovasandoval@gmail.com>
2020-10-08 17:43:57 -0500

    yarn issue and rubocop fixed

--- app/models/images/create_image.rb
+++ app/models/images/create_image.rb
@@ -1,14 +1,17 @@
-module Images
-    # Service object create Image
-    class CreateImage
-        def initialize(params)
-            @params = params
-        end
+# frozen_string_literal: true
 
-        def save
-            @image = Image.new @params
-            @image.save
-            @image #no se para que sirve
-        end
+
+module Images
+  # Service object create Image
+  class CreateImage
+    def initialize(params)
+      @params = params
     end
+
+    def save
+      @image = Image.new @params
+      @image.save
+      @image
+    end
+  end
 end
